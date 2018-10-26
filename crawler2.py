4efd2454c8e5b6a7af8a7f2a4e7a593773fbdb95
jsm <sang93423@gmail.com>
2018-10-26 16:46:54 +0900

    webtoon_crawler 함수명을 get_info로 바꿔주고 중복되는 부분을 함수화 시킴

--- crawler2.py
+++ crawler2.py
@@ -7,23 +7,15 @@ from bs4 import BeautifulSoup
 class Webtoon:
     def __init__(self, webtoon_id):
         self.webtoon_id = webtoon_id
-        # webtoon 속성 채우기 위해 webtoon_crawler() 실행
-        # webtoon_crawler 함수 결과 dict()
+        # webtoon 속성 채우기 위해 get_info() 실행
+        # get_info 함수 결과 dict()
 
-        info = self.webtoon_crawler()
+        info = self.get_info()
         self.title = info['title']
         self.author = info['author']
         self.description = info['description']
 
-    def webtoon_crawler(self):
-
-        """
-        webtoon_id 매개변수를 이용하여
-        웹툰 title, author, description을 딕셔너리 형태로 return
-        :return: title, author, description 딕셔너리로
-        """
-
-        # HTML파일을 저장하거나 불러올 경로
+    def get_html(self):
         file_path = 'data/episode_list-{webtoon_id}.html'.format(webtoon_id=self.webtoon_id)
         # HTTP요청을 보낼 주소
         url_episode_list = 'http://comic.naver.com/webtoon/list.nhn'
@@ -37,10 +29,22 @@ class Webtoon:
             # 저장되어 있다면 해당 파일을 읽어서 html변수에 할당
             html = open(file_path, 'rt').read()
         else:
+            # 저장되어 있지 않다면, 해당 파일을 읽어서 html변수에 할당
             response = requests.get(url_episode_list, params)
+            # 요청 응답객체의 text속성값을 html변수에 할당
             html = response.text
+            # 받은 텍스트 데이터를 HTML파일로 저장
             open(file_path, 'wt').write(html)
+        return html
 
+    def  (self):
+
+        """
+        webtoon_id 매개변수를 이용하여
+        웹툰 title, author, description을 딕셔너리 형태로 return
+        :return: title, author, description 딕셔너리로
+        """
+        html = self.get_html()
         soup = BeautifulSoup(html, 'lxml')
 
         h2_title = soup.select_one('div.detail > h2')
@@ -48,6 +52,8 @@ class Webtoon:
         author = h2_title.contents[1].get_text(strip=True)
         description = soup.select_one('div.detail > p').get_text(strip=True)
 
+        # webtoon title, author, description
+        # 딕셔너리 형태로 return
         webtoon_dict = {
             'title': title,
             'author': author,
